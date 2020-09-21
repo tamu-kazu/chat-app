5500279095b486e890b00434f5c9107a0cc7ff29
tamu-kazu <71427147+tamu-kazu@users.noreply.github.com>
2020-09-21 16:48:44 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,18 @@
 class MessagesController < ApplicationController
   
+  def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
+  end
+
   def create
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
     if @message.save
       redirect_to room_messages_path(@room)
     else
+      @messages = @room.messages.includes(:user)
       render :index
     end
   end
@@ -15,5 +22,4 @@ class MessagesController < ApplicationController
  def message_params
    params.require(:message).permit(:content).merge(user_id: current_user.id)
  end
-
 end
